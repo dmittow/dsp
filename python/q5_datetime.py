7888401c324e9208c1333b3b5e2cf2eec19de8df
dmittow <33236309+dmittow@users.noreply.github.com>
2017-11-02 11:22:05 -0700

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,27 @@
 # Hint:  use Google to find python function
 
+import numpy
+import pandas
+from datetime import datetime as dt
+from dateutil.parser import parse
+
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_start_0 = '01-02-2013'    
+date_stop_0 = '07-28-2015'
+
+diff_0 = dt.date(dt.strptime(date_stop_0, '%m-%d-%Y')) - dt.date(dt.strptime(date_start_0, '%m-%d-%Y'))
+print(diff)
 
 ####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+date_start_1 = '12312013'  
+date_stop_1 = '05282015' 
+
+diff_1 = dt.date(dt.strptime(date_stop_1, '%m%d%Y')) - dt.date(dt.strptime(date_start_1, '%m%d%Y'))
+print(diff_1)
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_start_2 = '15-Jan-1994'      
+date_stop_2 = '14-Jul-2015' 
+
+diff_2 = dt.date(dt.strptime(date_stop_2, '%d-%b-%Y')) - dt.date(dt.strptime(date_start_2, '%d-%b-%Y'))
+print(diff_2)
