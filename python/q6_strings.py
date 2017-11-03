f9917a89927b6fc60f7103ca53fb0cc4c0ba071b
dmittow <33236309+dmittow@users.noreply.github.com>
2017-11-03 13:28:20 -0700

    Update q6_strings.py

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -1,7 +1,11 @@
 # Based on materials copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 
-
+# Given an int count of a number of donuts, return a string of the
+# form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word
+# 'many' instead of the actual count.
+    
 def donuts(count):
     if count >= 10: 
         cnt_str = 'many' 
@@ -9,16 +13,11 @@ def donuts(count):
         cnt_str = str(count)
     return 'Number of donuts: ' + cnt_str
 
-    #>>> donuts(4)
-    #'Number of donuts: 4'
-    #>>> donuts(9)
-    #'Number of donuts: 9'
-    #>>> donuts(10)
-    #'Number of donuts: many'
-    #>>> donuts(99)
-    #'Number of donuts: many'
 
-
+#Given a string s, return a string made of the first 2 and the last
+#2 chars of the original string, so 'spring' yields 'spng'.
+#However, if the string length is less than 2, return instead the
+#empty string.
 
 def both_ends(s):
     if len(s) >= 2:
@@ -27,47 +26,30 @@ def both_ends(s):
         b_ends = ''
     return b_ends
 
-    #>>> both_ends('spring')
-    #'spng'
-    #>>> both_ends('Hello')
-    #'Helo'
-    #>>> both_ends('a')
-    #''
-    #>>> both_ends('xyz')
-    #'xyyz'
+#Given a string s, return a string where all occurences of its
+#first char have been changed to '*', except do not change the
+#first char itself. e.g. 'babble' yields 'ba**le' Assume that the
+#string is length 1 or more.
 
 
 def fix_start(s):
     temp = ['*' if c == s[0] else c for c in s]
     return s[0] + ''.join(temp[1:])
-    raise NotImplementedError
 
-    #>>> fix_start('babble')
-    #'ba**le'
-    #>>> fix_start('aardvark')
-    #'a*rdv*rk'
-    #>>> fix_start('google')
-    #'goo*le'
-    #>>> fix_start('donut')
-    #'donut'
+
    
-
+#Given strings a and b, return a single string with a and b
+#separated by a space '<a> <b>', except swap the first 2 chars of
+#each string. Assume a and b are length 2 or more.
 
 
 def mix_up(a, b):
     return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
-
-#     >>> mix_up('mix', 'pod')
-#     'pox mid'
-#     >>> mix_up('dog', 'dinner')
-#     'dig donner'
-#     >>> mix_up('gnash', 'sport')
-#     'spash gnort'
-#     >>> mix_up('pezzy', 'firm')
-#     'fizzy perm'
-
-
+#     Given a string, if its length is at least 3, add 'ing' to its end.
+#     Unless it already ends in 'ing', in which case add 'ly' instead.
+#     If the string length is less than 3, leave it unchanged. Return
+#     the resulting string.
 
 def verbing(s):
     if len(s) < 3:
@@ -76,45 +58,19 @@ def verbing(s):
         if s[-3:] == 'ing':
             return s + 'ly'
         else: return s + 'ing'
-#     """
-#     Given a string, if its length is at least 3, add 'ing' to its end.
-#     Unless it already ends in 'ing', in which case add 'ly' instead.
-#     If the string length is less than 3, leave it unchanged. Return
-#     the resulting string.
 
-#     >>> verbing('hail')
-#     'hailing'
-#     >>> verbing('swiming')
-#     'swimingly'
-#     >>> verbing('do')
-#     'do'
-#     """
-    raise NotImplementedError
-
+#Given a string, find the first appearance of the substring 'not'
+#and 'bad'. If the 'bad' follows the 'not', replace the whole
+#'not'...'bad' substring with 'good'. Return the resulting string.
+#So 'This dinner is not that bad!' yields: 'This dinner is
+#good!'
 
 import re
 
 def not_bad(s):
     return re.sub(r"not.*bad", "good", s)
 
-#     >>> not_bad('This movie is not so bad')
-#     'This movie is good'
-#     >>> not_bad('This dinner is not that bad!')
-#     'This dinner is good!'
-#     >>> not_bad('This tea is not hot')
-#     'This tea is not hot'
-#     >>> not_bad("It's bad yet not")
-#     "It's bad yet not"
-#     """
-    raise NotImplementedError
 
-
-import math
-
-def front_back(a, b):
-    half = lambda a: math.ceil(len(a)/2)
-    return a[:half(a)] + b[:half(b)] + a[half(a):] + b[half(b):]
-#     """
 #     Consider dividing a string into two halves. If the length is even,
 #     the front and back halves are the same length. If the length is
 #     odd, we'll say that the extra char goes in the front half. e.g.
@@ -122,10 +78,9 @@ def front_back(a, b):
 #     strings, a and b, return a string of the form a-front + b-front +
 #     a-back + b-back
 
-#     >>> front_back('abcd', 'xy')
-#     'abxcdy'
-#     >>> front_back('abcde', 'xyz')
-#     'abcxydez'
-#     >>> front_back('Kitten', 'Donut')
-#     'KitDontenut'
-#     """
+import math
+
+def front_back(a, b):
+    half = lambda a: math.ceil(len(a)/2)
+    return a[:half(a)] + b[:half(b)] + a[half(a):] + b[half(b):]
+
