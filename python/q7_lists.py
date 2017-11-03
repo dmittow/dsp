2f22c8624d809b9f667ba7b5343f7e41e61a833c
dmittow <33236309+dmittow@users.noreply.github.com>
2017-11-02 17:14:10 -0700

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -3,18 +3,20 @@
 
 
 def match_ends(words):
-    """
-    Given a list of strings, return the count of the number of strings
-    where the string length is 2 or more and the first and last chars
-    of the string are the same.
-
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
-    """
+    match_words = [w for w in words if len(w) >= 2 and w[0] == w[-1]]
+    return len(match_words)
+    #return len(words)
+#     """
+#     Given a list of strings, return the count of the number of strings
+#     where the string length is 2 or more and the first and last chars
+#     of the string are the same.
+#     >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
+#     3
+#     >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
+#     2
+#     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
+#     1
+#    """
     raise NotImplementedError
 
 
