6ad7a50cead976a931c68df93e105cd1b81a7cd4
dmittow <33236309+dmittow@users.noreply.github.com>
2017-11-06 16:07:04 -0800

    commit through browser interface, starting point

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,27 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+
+with open('faculty.csv','r') as f: 
+    f_reader = csv.reader(f)
+    next(f_reader)
+    
+    degrees = []
+    titles = []
+    emails = []
+    domains = []
+    
+    for line in f_reader:
+        deg_std = str.split(str.replace(line[1], '.','').lower(),' ')
+        degrees = degrees + [d.upper() for d in deg_std if d != '' and d.isalpha()]
+        titles = titles + [str.replace(line[2], ' of Biostatistics', '')]
+        emails = emails + [e for e in line if re.match(r'.*@.*',e)]
+        domains = domains + [d[d.index('@') + 1:] for d in line if re.match(r'.*@.*',d)]
+        
+    deg_count_dict = {d:degrees.count(d) for d in degrees}
+    title_count_dict = {t:titles.count(t) for t in titles}
+    unique_domains = set(domains)
+    
+print(deg_count_dict)
+print(title_count_dict)
+print(emails)
+print(unique_domains)
